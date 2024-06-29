fix: Fix precision-recall curve

AI_PROJECT/model/existent_model.py
@@ -1,14 +1,12 @@
 import torch
 import torch.utils
 import torch.utils.data
-import torchvision
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from model.utils import get_cuda_device, Averager, calculate_metrics, create_precision_recall_curve, nms_filter_boxes
 from dataset.dataset import visualize_images
 from dataset.utils.transformations import collate_function
 from torchmetrics.detection import MeanAveragePrecision
 import os
-import numpy as np
+from model.utils import (get_cuda_device, Averager, nms_on_output_dictionary,
+                         nms_filter_boxes, evaluate_predictions)
 
 
 def checkpoint(model, filedir, epoch):
@@ -27,7 +25,7 @@ def train_model(train_data_loader, valid_data_loader,
     optimizer = torch.optim.Adam(params, lr=0.001)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.5)
     mAP = MeanAveragePrecision(box_format="xyxy", iou_type="bbox", class_metrics=True)
-    num_epochs = 40
+    num_epochs = 30
 
     loss_hist = Averager()
     itr = 1
@@ -63,12 +61,10 @@ def train_model(train_data_loader, valid_data_loader,
                 print("Validation images: ", len(images))
                 images = list(image.to(device) for image in images)
                 valid_targets = [{k: v.to(torch.device("cuda")) for k, v in t.items()} for t in valid_targets]
-                # new_valid_targets = valid_targets.copy()
-                # for item in new_valid_targets: print(item['labels'].shape)
-                # for item in new_valid_targets: item['scores'] = torch.ones(size=item['labels'].shape)
                 output = model(images)
                 output = [{k: v.to(torch.device("cuda")) for k, v in t.items()} for t in output]
-                mAP.update(preds=output, target=valid_targets)
+                filtered_output = nms_on_output_dictionary(output)
+                mAP.update(preds=filtered_output, target=valid_targets)
                 metrics = mAP.compute()
                 print(metrics)
             # update the learning rate
@@ -85,40 +81,44 @@ def inference_test(weights_file, model, test_dataloader):
     device = get_cuda_device()
     model.to(device)
     model.eval()
-    for images, targets in test_dataloader:
+    for images, _ in test_dataloader:
         images = list(image.to(device) for image in images)
         output = model(images)
         for ind, image in enumerate(images):
             boxes = output[ind]['boxes'].data.cpu()
             labels = output[ind]['labels'].data.cpu()
             scores = output[ind]['scores'].data.cpu()
-            filtered_boxes, filtered_labels = nms_filter_boxes(boxes, scores, labels, 0.5)
+            filtered_boxes, filtered_labels, _ = nms_filter_boxes(boxes, scores, labels, 0.15)
             new_image = image.data.cpu()
             visualize_images(new_image, filtered_boxes, filtered_labels, inference=True)
 
 
-def model_test_metrics(weights_file, model, test_dataloader):
-    metric_counter = 0
-    class_thresholds = np.arange(start=0.0, step=0.05, stop=1.05)
-    num_samples = 0
+def get_inference_and_metrics(weights_file, model, data_loader, num_classes, iou_threshold=0.15):
     model.load_state_dict(torch.load(weights_file))
     device = get_cuda_device()
     model.to(device)
     model.eval()
-    for images, targets in test_dataloader:
-        images = list(image.to(device) for image in images)
-        num_samples += len(images)
-        output = model(images)
-        for ind, image in enumerate(images):
-            predictions = dict()
-            predictions["boxes"] = output[ind]['boxes'].data.cpu()
-            predictions["labels"] = output[ind]['labels'].data.cpu()
-            predictions["scores"] = output[ind]['scores'].data.cpu()
-            if metric_counter == 0:
-                metric_counter = calculate_metrics(predictions, targets[ind], class_thresholds)
-            else:
-                new_values = calculate_metrics(predictions, targets[ind], class_thresholds)
-                for key in new_values.keys():
-                    metric_counter[key] += new_values[key]
+    device = get_cuda_device()
+    y_true = {i: [] for i in range(1, num_classes+1)}
+    y_scores = {i: [] for i in range(1, num_classes+1)}
 
-    create_precision_recall_curve(class_thresholds, metric_counter)
+    with torch.no_grad():
+        for images, targets in data_loader:
+            images = [image.to(device) for image in images]
+            outputs = model(images)
+            
+            for i, output in enumerate(outputs):
+                gt_boxes = targets[i]['boxes'].cpu().numpy()
+                gt_labels = targets[i]['labels'].cpu().numpy()
+                
+                pred_boxes = output['boxes'].cpu().numpy()
+                pred_scores = output['scores'].cpu().numpy()
+                pred_labels = output['labels'].cpu().numpy()
+
+                tp, fp, fn = evaluate_predictions(gt_boxes, gt_labels, pred_boxes, pred_labels, pred_scores, iou_threshold)
+                
+                for class_id in range(1, num_classes+1):
+                    y_true[class_id].extend([1] * tp[class_id] + [0] * fp[class_id])
+                    y_scores[class_id].extend(pred_scores[pred_labels == class_id])
+
+    return y_true, y_scores

AI_PROJECT/model/utils.py
@@ -2,6 +2,7 @@ import torch
 from torchvision.ops import box_iou, nms
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import precision_recall_curve
 
 
 def get_cuda_device():
@@ -30,92 +31,62 @@ class Averager:      # Return the average loss
         self.iterations = 0.0
 
 
-def counter_metrics_per_label(num_classes, num_threhsolds):
-    '''Dictionary to store TP, FP and FN'''
-    counter = dict()
-    for i in range(1, num_classes + 1):
-        counter[i] = np.zeros(shape=(num_threhsolds, 3))
-    return counter
-
-
-def precision_recall_dict(num_thresholds: int, num_classes: int = 3):
-    # 0: Precision, 1: Recall
-    pr_vector = dict()
-    for i in range(1, num_classes + 1):
-        pr_vector[i] = np.zeros(shape=(num_thresholds, 2))
-    return pr_vector
-
-
-def tp_fp_on_different_thresholds(pred_label, pred_score, num_classes, thresholds):
-    tp_fp_vector = counter_metrics_per_label(3, thresholds.shape[0])
-    for ind, threshold in enumerate(thresholds):
-        for id_class in range(1, num_classes + 1):
-            if pred_score > threshold and pred_label == id_class:
-                tp_fp_vector[id_class][ind, 0] = 1
-            else:
-                tp_fp_vector[id_class][ind, 1] = 1
-    return tp_fp_vector
-
-
-def get_precision_recall(thresholds_counter: dict, num_thresholds):
-    precision_recall_vector = precision_recall_dict(num_thresholds=num_thresholds)
-    for key in thresholds_counter.keys():
-        for ind, row in enumerate(thresholds_counter[key]):
-            precision_recall_vector[key][ind, 0] = row[0] / (row[0] + row[1]) if row[0] + row[1] > 0 else 0
-            precision_recall_vector[key][ind, 1] = row[0] / (row[0] + row[2]) if row[0] + row[2] > 0 else 0
-    return precision_recall_vector
-
-
-def create_precision_recall_curve(thresholds, thresholds_counter):
-    styles = ["-b", "-g", "-r"]
-    precision_recall_vector = get_precision_recall(thresholds_counter, thresholds.shape[0])
-    for ind, key in enumerate(precision_recall_vector.keys()):
-        precision = precision_recall_vector[key][:, 0]
-        recall = precision_recall_vector[key][:, 1]
-        plt.plot(precision, recall, styles[ind], label=f"Class {key}")
-    plt.xlabel("Precision")
-    plt.ylabel("Recall")
-    plt.axis((0, 1, 0, 1))
-    plt.title("Precision-Recall Curve")
-    plt.legend(loc=4)
-    plt.show()
-
-
-def calculate_metrics(predictions, groundtruth, class_thresholds, iou_threshold: float = 0.5):
-
-    thresholds_counter = counter_metrics_per_label(3, class_thresholds.shape[0])
-    iou_matrix = box_iou(predictions["boxes"], groundtruth["boxes"])
-    gt_found = []
-    for num_row, row in enumerate(iou_matrix):
-        greater_iou = row >= iou_threshold
-        indexes = greater_iou.nonzero()
-        if indexes.nelement() > 0:
-            if indexes.nelement() > 1:
-                print("MOre indexes found")
-            for index in indexes:
-                # print("Number of box: ", num_row, " IOU with GT BOX: ", index.item())
-                pred_label = predictions["labels"][num_row]
-                pred_score = predictions["scores"][num_row]
-                # gt_label = groundtruth["labels"][index.item()]
-                gt_found.append(index.item())
-                pred_tp_fp = tp_fp_on_different_thresholds(pred_label, pred_score, 3, class_thresholds)
-                for key in thresholds_counter.keys():
-                    thresholds_counter[key] += pred_tp_fp[key]
-        else:
-            for key in thresholds_counter.keys():
-                thresholds_counter[key][:, 1] += 1
-
-    gt_found = set(gt_found)
-    fn_diff = len(groundtruth["boxes"]) - len(gt_found)
-    fn = fn_diff if fn_diff >= 0 else 0
-    for key in thresholds_counter.keys():
-        thresholds_counter[key][:, 2] += fn
-
-    return thresholds_counter
-
-
 def nms_filter_boxes(result_boxes, result_scores, result_labels, iou_threshold):
     nms_ids = nms(result_boxes, result_scores, iou_threshold)
     filtered_boxes = result_boxes.index_select(0, nms_ids)
     filtered_labels = result_labels.index_select(0, nms_ids)
-    return filtered_boxes, filtered_labels
+    filtered_scores = result_scores.index_select(0, nms_ids)
+    return filtered_boxes, filtered_labels, filtered_scores
+
+
+def nms_on_output_dictionary(output, iou_threshold=0.5):
+    filtered_dictionary = []
+    for item in output:
+        filtered_result = nms_filter_boxes(item['boxes'], item['scores'], item['labels'], iou_threshold)
+        item['boxes'] = filtered_result[0]
+        item['labels'] = filtered_result[1]
+        item['scores'] = filtered_result[2]
+        filtered_dictionary.append(item)
+    return filtered_dictionary
+
+
+def evaluate_predictions(gt_boxes, gt_labels, pred_boxes, pred_labels, pred_scores, iou_threshold=0.5):
+    num_classes = len(np.unique(gt_labels))
+    tp = {i: 0 for i in range(1, num_classes+1)}
+    fp = {i: 0 for i in range(1, num_classes+1)}
+    fn = {i: 0 for i in range(1, num_classes+1)}
+
+    for class_id in range(1, num_classes+1):
+        gt_mask = gt_labels == class_id
+        pred_mask = pred_labels == class_id
+
+        if gt_mask.sum() == 0:
+            continue
+
+        ious = box_iou(torch.tensor(pred_boxes[pred_mask]), torch.tensor(gt_boxes[gt_mask])).numpy()
+
+        for i in range(len(gt_boxes[gt_mask])):
+            max_iou = ious[:, i].max() if ious.shape[1] > i else 0
+            if max_iou >= iou_threshold:
+                tp[class_id] += 1
+            else:
+                fn[class_id] += 1
+
+        fp[class_id] += len(pred_boxes[pred_mask]) - tp[class_id]
+
+    return tp, fp, fn
+
+
+def create_PRC(y_true, y_scores, num_classes):
+    styles = ["-b", "-g", "-r"]
+    class_name = ["Entity", "Attribute", "Relation"]
+    for class_id in range(1, num_classes+1):
+        precision, recall, thresholds = precision_recall_curve(y_true[class_id], y_scores[class_id])
+        
+        plt.plot(recall, precision, styles[class_id - 1], label=f'Class {class_name[class_id - 1]}')
+        
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall Curve for Each Class')
+    plt.legend(loc=4)
+    plt.show()