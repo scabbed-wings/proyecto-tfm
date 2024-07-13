c557a06d6089a92fbcfd149d04fab0071031f509
scabbed_wings <scabbed.wings.demon@gmail.com>
2024-07-13 14:01:08 +0200

    feat: Save crops and annotations on output path

--- data_generator/create_relation_data.py
+++ data_generator/create_relation_data.py
@@ -4,19 +4,30 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import os
-from utils.generate_relational_image import crop_relational_image
+import json
+from utils.generate_relational_images import crop_relational_image
 
 if __name__ == "__main__":
     path = "data_generator/prueba"
     output_parent = Path("data_generator/relational_images")
+    crops_path = Path(os.path.join(output_parent, 'images'))
+    labels_path = Path(os.path.join(output_parent, 'labels.json'))
     output_parent.mkdir(parents=True, exist_ok=True)
+    crops_path.mkdir(parents=True, exist_ok=True)
     images_list = glob(path + "/*.png")
+    dataset_labels = []
+    
     for image_name in images_list:
         print("Processing image: ", image_name)
         image_path = Path(image_name)
-        output_path = Path(os.path.join(output_parent), image_path.name)
+        output_path = Path(os.path.join(crops_path), image_path.name)
         csv_path = f"{image_path.parent}/{image_path.stem}.csv"
         image = Image.open(image_path).convert('L')
         image = np.array(image)
         df = pd.read_csv(csv_path, sep=";")
-        crop_relational_image(image, output_path, df)
+        image_crops_labels = crop_relational_image(image, output_path, df)
+        dataset_labels += image_crops_labels
+    
+    with open(labels_path, 'w', encoding='utf-8') as f:
+        json.dump(dataset_labels, f, indent=4)
+    
--- data_generator/utils/generate_relational_images.py
+++ data_generator/utils/generate_relational_images.py
@@ -1,7 +1,15 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import cv2
+from pathlib import Path
 
+
+def crop_labels(crop_name, label, bbox_source, bbox_target):
+    return {
+        "image_name": str(crop_name.name),
+        "label": label,
+        "bbox1": bbox_source,
+        "bbox2": bbox_target
+    }
 
 def process_related_objects(related_objects_string: str):
     new_list = related_objects_string.strip('][').split(',')
@@ -36,15 +44,14 @@ def resize_boxes_2_crop(xmin, ymin, bbox1, bbox2):
     return bbox1, bbox2
 
 
-def crop_relations(image, bbox_origin, valid_objects, 
-                   origin_id, processed_relations, output_path):
+def crop_relations(image, bbox_origin, valid_objects, origin_id, processed_relations, output_path):
+    labels = []
     for index in range(len(valid_objects)):
         row = valid_objects.iloc[index]
         relation = [row['id'], origin_id]
         if not relation_is_processed(relation, processed_relations):
             processed_relations.append(relation)
             crop_path = f"{output_path.parent}/{output_path.stem}_{len(processed_relations)}.png"
-            print("Crop path: ", crop_path)
             bbox_target = process_bounding_box(row['x_min'], row['y_min'], row['x_max'], row['y_max'])
             target_relations = process_related_objects(row['related_objects'])
             label = 1 if origin_id in target_relations else 0
@@ -53,18 +60,22 @@ def crop_relations(image, bbox_origin, valid_objects,
             bbox_source, bbox_target = resize_boxes_2_crop(xmin, ymin, bbox_origin,
                                                         bbox_target)
             copy_binary_image = copy_binary_image[ymin:ymax, xmin:xmax]
-            # cv2.imwrite(crop_path, copy_binary_image)
-            # plt.imshow(copy_binary_image, 'gray')
-            # plt.show()
-    #return copy_binary_image, bbox_source, bbox_target
+            crop_label = crop_labels(Path(crop_path), label, bbox_source, bbox_target)
+            labels.append(crop_label)
+            
+            cv2.imwrite(crop_path, copy_binary_image)
+    return labels
         
 
 def crop_relational_image(image, output_path, labels_df):
     processed_relations = []
+    processed_labels = []
     for index in range(len(labels_df)):
         row = labels_df.iloc[index]
         bbox_source = process_bounding_box(row['x_min'], row['y_min'], row['x_max'], row['y_max'])
         valid_objects = labels_df.loc[labels_df['class'] != row['class']]
-        crop_relations(image, bbox_source, valid_objects, row['id'], 
-                       processed_relations, output_path)
+        crop_labels = crop_relations(image, bbox_source, valid_objects, row['id'],
+                                     processed_relations, output_path)
+        processed_labels += crop_labels
+    return processed_labels
         
