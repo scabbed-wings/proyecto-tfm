cbbf86f3e3c2e9a7a9adc7d4c04c6fd3ea4f1db8
scabbed_wings <scabbed.wings.demon@gmail.com>
2025-03-07 22:15:52 +0100

    Visualize annotations with IDS and related objects

--- deep_learning/visualize_dataset.py
+++ deep_learning/visualize_dataset.py
@@ -7,12 +7,15 @@ import numpy as np
 import cv2
 
 
-def show_image(image_name: str, bboxes: np.ndarray, labels: np.ndarray):
+def show_image(image_name: str, bboxes: np.ndarray, labels: np.ndarray, ids: np.ndarray, related_objects: np.ndarray):
     image = cv2.imread(image_name)
     for i in range(bboxes.shape[0]):
         color = color_selector(labels[i])
         cv2.rectangle(image, (bboxes[i, 0], bboxes[i, 1]),
                       (bboxes[i, 2], bboxes[i, 3]), color, 2)
+        centroid = (int(bboxes[i, 0]), int(bboxes[i, 1]))
+        cv2.putText(image, f"id {ids[i]}-{related_objects[i]}",
+                    centroid, cv2.FONT_HERSHEY_COMPLEX, 0.7, (20, 117, 255), 2)
     cv2.imshow("Annotations", image)
     cv2.waitKey(0)
 
@@ -28,10 +31,12 @@ def visualize_dataset(dataset_path: str):
             annotations = pd.read_csv(csv_path, sep=";", index_col=0)
             bboxes = annotations[["x_min", "y_min", "x_max", "y_max"]].values
             labels = annotations["class"].values
-            show_image(image_name, bboxes, labels)
+            element_ids = annotations["id"].values
+            related_objects = annotations["related_objects"].values
+            show_image(image_name, bboxes, labels, element_ids, related_objects)
     else:
         raise NotADirectoryError(f'{dataset_path} is not a directory')
 
 
 if __name__ == "__main__":
-    visualize_dataset(r'data_generator\real_images')
+    visualize_dataset(r'data_generator\real_sample_images')
